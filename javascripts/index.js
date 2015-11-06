24cd99f06ff907e39c2b1e12944246b05f203854
mfoonirlee <hong.li@Ctrip.com>
2015-11-07 00:30:17 +0800

    update css add list update readme for update todos

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,4 @@
+//做路由切换时需要改为define
 require(['vue', 'Zepto'], function (Vue, $) {
 	var Index = new Vue({
 		el: '#j_index',
---
+++ javascripts/list.js
@@ -0,0 +1,31 @@
+//做路由切换时需要改为define
+require(['vue', 'Zepto'], function (Vue, $) {
+	var List = new Vue({
+		el: '#j_list',
+		data: {
+			msg_pos: '光大会展中心',
+			msg_tab_left: '今日预定',
+			msg_tab_right: '明日预定',
+			price: '',
+			num: '',
+			plist: []
+		},
+		methods:{
+			chooseTab: function (e) {
+				var $target = $(e.currentTarget),
+					$bar = $(this.$el).find('#j_bar');
+
+				//处理字体变色
+				if(!$target.hasClass('current')){
+					$target.addClass('current').siblings().removeClass('current');
+				}
+				//处理边框
+				if($target.data('type')){
+					$bar.addClass('tab-move');
+				}else{
+					$bar.removeClass('tab-move');
+				}
+			}
+		}
+	});
+})
