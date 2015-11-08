12818a9d7f1b19f2287ab8cfcfb581613f6aacba
YANG-RUI <yzyangrui@gmail.com>
2015-11-08 22:13:27 +0800

    booking page init

---
+++ javascripts/booking.js
@@ -0,0 +1,10 @@
+//做路由切换时需要改为define
+require(['vue', 'Zepto'], function (Vue, $) {
+    var Index = new Vue({
+        el: '#j_booking',
+        data: {
+        },
+        methods: {
+        }
+    })
+});
