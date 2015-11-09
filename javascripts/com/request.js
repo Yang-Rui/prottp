214303d523b4b41a0ac974521ff856b2b980f929
YANG-RUI <yzyangrui@gmail.com>
2015-11-09 23:22:44 +0800

    refine request function

--- javascripts/com/request.js
+++ javascripts/com/request.js
@@ -10,10 +10,40 @@ define(['Zepto'], function ($) {
         //rreqName即请求名，在这里维护
         reqHash: {
             //查询位置
-            reqLocation: 'unsecure/buildings'
+            reqLocation: ['GET', 'unsecure/buildings'],
+            //厨房信息
+            kitchenInfo: ['GET', 'unsecure/kitchen/{kitchenId}']
+        },
+        /*
+            发送网络请求
+            @param object {key: value}
+            @success function
+            @error function
+        */
+        execute: function (reqName, params, success, error) {
+            var _self = this,
+                reqUrl = this.getReqURL(reqName, params);
+
+            if (!reqUrl) {
+                return;
+            }
+
+            $.ajax({
+                    type: _self.reqHash[reqName][0],
+                    url: reqUrl,
+                    dataType: 'json',
+                    content: self,
+                    data: _self.reqHash[reqName][0] == 'POST' ? params : {},
+                    success: function (data) {
+                        success && typeof success == 'function' && success.call(this);
+                    },
+                    error: function (data) {
+                        error && typeof error == 'function' && error.call(this);
+                    },
+                });
         },
         /*检测网络环境*/
-        getEnviroment: function () {
+        getEnvironment: function () {
             if(location.host.indexOf('localhost') > -1){
                 return 'dev';
             }else{
@@ -21,12 +51,27 @@ define(['Zepto'], function ($) {
             }
         },
         /*获取请求的完整url*/
-        getReqURL: function (reqName) {
+        getReqURL: function (reqName, params) {
             var url = '';
-            url = this.reqHost[this.getEnviroment()];
+            // url = this.reqHost[this.getEnvironment()];
+            url = 'http://localhost:8088';
+            //如果找不到配置报错
+            if (!this.reqHash[reqName]) {
+                console.error('No url configured found.');
+                return;
+            }
 
-            url += '/' + this.reqHash[reqName];
-
+            url += '/' + this.reqHash[reqName][1];
+            //GET 请求拼写url
+            if (this.reqHash[reqName][0] == 'GET') {
+                for (key in params) {
+                    if (url.indexOf('{' + key + '}') > -1) {
+                        url = url.replace('{' + key + '}', params[key]);
+                    } else {
+                        url += ((url.indexOf('?') ? '?' : '&') + key + '=' + params[key]);
+                    }
+                }
+            }
             return url;
         }
     };
